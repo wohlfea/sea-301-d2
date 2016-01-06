2e66ac38ac5feb60b914c60870ca8a74891fbc4a
wohlfea <wohlfea@gmail.com>
2016-01-05 16:15:45 -0800

    most Todos have been completed

--- class-02-jquery-and-events/pair-assignment/aj-andy/scripts/article.js
+++ class-02-jquery-and-events/pair-assignment/aj-andy/scripts/article.js
@@ -16,6 +16,7 @@ Article.prototype.toHtml = function() {
     $newArticle.addClass('draft');
   }
   $newArticle.attr('data-category', this.category);
+  $newArticle.attr('data-author', this.author);
   // TODO: Use jQuery to also add the author name as a data-attribute of the newly cloned article.
   //       Doing so will allow us to use selectors to target articles, based on who wrote them.
 
