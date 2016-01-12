497b4599cd2b8675d0bbe48d138a792c3b4bed58
a-she <al@alswebdesigns.net>
2016-01-12 03:07:49 -0800

    rm'd stray copy of AJAX demo code; see code in class-06.

--- class-07-functional-programming/lecture/ajaxAsyncDemo/scripts/demo.js
+++
@@ -1,9 +0,0 @@
-// Run this and demo execution order.
-// Try different methods to ensure correct execution.
-$.get('dialog/speech.txt', function (data) {
-  console.log(data);
-  console.log('I should come first.');
-});
-
-console.log('I should come second.');
-console.log('I should come third!');
