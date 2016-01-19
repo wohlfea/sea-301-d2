b6646f14d789f08823e2d053250e26b72653099f
wohlfea <wohlfea@gmail.com>
2016-01-19 13:01:28 -0800

    repo.js added

--- class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repo.js
+++ class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repo.js
@@ -6,6 +6,12 @@
   repos.requestRepos = function(callback) {
     // TODO: How would you like to fetch your repos? Don't forget to call the callback.
 
+    // Fetch repo's with ajax - 1st requirement
+    // Write a success function to do something
+    var qs = '?per_page=100&sort=updated';
+    $.get('/github/users/bnates/repos' + qs).done(function(data, msg, xhr){
+      repos.all = data;
+    }).done(callback)
   };
 
   // DONE: Model method that filters the full collection for repos with a particular attribute.
